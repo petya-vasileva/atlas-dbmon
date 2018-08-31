5655769e735e57111c12ff8abf1261a747f39963
Benjamin Scheer <benjamin.scheer@cern.ch>
2018-08-31 17:07:41 +0200

    worked on sql script to copy functions and types to production

---
+++ SQL/COPY_TO_PRODUCTION.sql
@@ -0,0 +1,29 @@
+-- ## 1.## CREATE SCRIPT for Types & Functions.
+-- Execute code below as Script! (F5)
+
+spool DBMON_functions.sql
+
+SELECT 
+   REPLACE (DBMS_METADATA.GET_DDL( object_type, object_name, 'DBMON_SCHEMA'), 'CREATE OR REPLACE TYPE "DBMON_SCHEMA"', 'CREATE OR REPLACE TYPE "DBMON_SCHEMA_DEV"') as definition
+FROM 
+   user_objects 
+WHERE 
+   object_type IN ('TYPE')
+ORDER BY substr(object_name, -3);
+
+-- Export from GUI
+-- Execute in batch or one by one
+
+--################################################################
+
+SELECT 
+   REPLACE (DBMS_METADATA.GET_DDL( object_type, object_name, 'DBMON_SCHEMA'), 'CREATE OR REPLACE TYPE "DBMON_SCHEMA"', 'CREATE OR REPLACE TYPE "DBMON_SCHEMA_DEV"') as definition
+FROM 
+   user_objects 
+WHERE 
+   object_type IN ('FUNCTION')
+ORDER BY OBJECT_ID;
+
+spool off
+
+--## 2.## RUN CREATED SCRIPT
